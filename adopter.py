f4f9787e391dcfacd4b0e9ac3efbb0c72e939705
Vihaan Mishra <vihaan.mishra@challoners.org>
2025-11-18 15:41:05 +0000

    Lesson 5 (Bebras)

--- adopter.py
+++ adopter.py
@@ -1,8 +1,15 @@
 import staff
+import pandas as pd 
 
 def adopter_login():
+    df = pd.read_csv("adopters.csv")
     adopter_id = input("What is your ID:")
-    if adopter_id in staff.df.adopters.csv:
-        id = staff.df[staff.df.adopter.csv == adopter_id]
+    if len(adopter_id) == 4:
+        df = df[df["AdopterID"]= adopter_id]
+    if len(df) == 1:
+        print("Login Successful. Adopter Menu")
+    else:
+        print("Login failed")
 
+    
 adopter_login()
